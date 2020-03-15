f224a80db54e110d856f3c459f7eebaeab4f3490
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-03-14 20:18:30 -0700

    Models folder with User.js exporting UserShema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+User = mongoose.model('user', UserShema);
+
+module.exports = User;
