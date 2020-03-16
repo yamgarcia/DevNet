7b68498f81ad2f7e7f4a0beaed90558b8c998498
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-03-15 17:30:14 -0700

    JWT implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -58,17 +60,33 @@ router.post(
         avatar,
         password
       });
-      // Encrypt password
+      // Encrypt/hash password
 
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
 
+      //save the user in the database
       await user.save();
 
       // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-      res.send('User registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecrete'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //   res.send('User registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
