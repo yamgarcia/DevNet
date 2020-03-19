eda57773b0df89af6737a1764dd062c37815f054
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-03-18 23:50:34 -0700

    Create & update profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+
+const { check, validationResult } = require("express-validator");
 
 /**
  * @route    GET api/profile/me
@@ -25,5 +28,91 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+/**
+ * @route    POST api/profile
+ * @desc     Create and update a user profile
+ * @access   Private
+ */
+
+//use auth and validation middleware
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    //checks for errors
+    !errors.isEmpty() && res.status(400).json({ errors: errors.array() });
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      instagram,
+      linkedin,
+      twitter
+    } = req.body;
+
+    //build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+
+    //build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (twitter) profileFields.social.twitter = twitter;
+
+    //use async await whenever use a mongoose method because it returns a promisse
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+      //create
+      profile = new Profile(profileFields);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
