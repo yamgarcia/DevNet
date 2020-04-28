0ee3a37570c38ea2ee2862b5faa412c7e7a2356e
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-04-28 10:47:41 -0700

    Auth middleware & JWT

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,21 +2,21 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 // Middleware function(req, res, next)
-module.exports = function(req, res, next) {
- //get token from geader
- const token = req.header("x-auth-token");
+module.exports = function (req, res, next) {
+  //get token from header
+  const token = req.header("x-auth-token");
 
- //Check if there is a token
- if (!token) {
-  return res.status(401).json({ msg: "Autorization denied. No token." });
- }
+  //Check if there is a token
+  if (!token) {
+    return res.status(401).json({ msg: "Autorization denied. No token." });
+  }
 
- // Verify token
- try {
-  const decoded = jwt.verify(token, config.get("jwtSecrete"));
-  req.user = decoded.user;
-  next();
- } catch (err) {
-  res.status(401).json({ msg: "Autorization denied. Token not valid" });
- }
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecrete"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Autorization denied. Token not valid" });
+  }
 };
