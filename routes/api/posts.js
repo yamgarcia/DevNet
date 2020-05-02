133663bdf1c33f91dbf9c957c5c652bd2f158e63
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-05-02 11:19:20 -0700

    Post Route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -43,4 +43,70 @@ router.post(
   }
 );
 
+/**
+ * @route    GET api/posts
+ * @desc     Get all posts
+ * @access   Private
+ *
+ */
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ data: -1 });
+    //-1 brings the most recent first
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route    GET api/posts/:id
+ * @desc     Get post by id
+ * @access   Private
+ *
+ */
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId")
+      return res.status(404).json({ msg: "Post not found" });
+
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route    DELETE api/posts/:id
+ * @desc     Delete post by id
+ * @access   Private
+ *
+ */
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
