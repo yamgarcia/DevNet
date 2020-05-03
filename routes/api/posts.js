f7813be823d970c10902ddd7519c1baf7ff3258d
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-05-02 20:11:52 -0700

    Post route finished - BACKEND DONE

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -85,7 +85,6 @@ router.get("/:id", auth, async (req, res) => {
  * @route    DELETE api/posts/:id
  * @desc     Delete post by id
  * @access   Private
- *
  */
 router.delete("/:id", auth, async (req, res) => {
   try {
@@ -105,6 +104,139 @@ router.delete("/:id", auth, async (req, res) => {
     if (err.kind === "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route    PUT api/posts/like/:id
+ * @desc     Like a post
+ * @access   Private
+ * *like
+ */
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    //get the post
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post has already been liked by the user
+    //.filter() takes a function that returns accordinly to the condition
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    //unshift adds to the beginning
+    post.likes.unshift({ user: req.user.id });
+    //save back to the db
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route    PUT api/posts/unlike/:id
+ * @desc     Unlike a post
+ * @access   Private
+ * *like
+ */
+
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not been liked" });
+    }
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route    POST api/posts/comment/:id
+ * @desc     Comment on a post
+ * @access   Private
+ * *comments
+ */
+router.post(
+  "/comments/:id",
+  [auth, [check("text", "Text is Required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+/**
+ * @route    DELETE api/posts/comment/:id/:comment_id
+ * @desc     Delete comment
+ * @access   Private
+ * *comments
+ */
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment) return res.status(404).json({ msg: "Comment not found" });
+
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
