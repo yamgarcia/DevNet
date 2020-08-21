ba8443d85d2761d83dfa8a007519bbc65e458749
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-08-21 10:08:19 -0700

    Update Profile action & reducer

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_REPOS,
 } from "../actions/types";
 
 /**
@@ -31,6 +33,12 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false,
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -42,6 +50,12 @@ export default function (state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
     default:
