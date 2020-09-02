cfc162cdcd1e25f7f3f68c92a687f2a424ed16ec
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-09-02 10:28:46 -0700

    Add posts page layout

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import PostItem from "../posts/PostItem";
 import { getPosts } from "../../actions/post";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -10,7 +11,26 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return <>{posts && <Spinner />}</>;
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h1 className='large text-primary'>Posts</h1>
+          <p className='lead'>
+            <i className='fas fa-user'></i>Welcome to the community
+          </p>
+          {/* Post form */}
+          <div className='posts'>
+            {posts.map((post) => (
+              <PostItem key={post._id} post={post} />
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 Posts.propTypes = {
