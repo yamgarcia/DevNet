b9807219271e68f099ea105eac8ec968bc01c9da
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-09-09 21:38:56 -0700

    heroku deploy prep

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 const app = express();
+
+const path = require("path");
 
 //connect database
 connectDB();
@@ -10,13 +12,23 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 //define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assetes in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
